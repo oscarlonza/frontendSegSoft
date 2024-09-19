5bf2095e85bda918b4cea16165f1e21eb820e023
rmartinezb14 <rmartinez@jamar.com>
2024-09-19 00:36:49 -0500

    login+registro+descarga de archivo
    login+registro+descarga de archivo

--- src/app/modules/cryptography/cryptography.component.ts
+++ src/app/modules/cryptography/cryptography.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cryptography',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './cryptography.component.html',
   styleUrl: './cryptography.component.scss'
 })
 export default class CryptographyComponent {
-
+  fb = inject(FormBuilder);
+  operation='Encriptar'
+  key=null
+  base64Image: any = null;
+  fileName: string | null = null;;
+  onFileSelected(event: Event): void {
+    const element = event.target as HTMLInputElement;
+    if (element && element.files && element.files.length) {
+      const file = element.files[0];
+      this.fileName = file.name; 
+      const reader = new FileReader();
+      reader.onload = () => {
+        const result = reader.result as string;
+        this.base64Image = result.replace(/^data:image\/[a-z]+;base64,/, '');
+        console.log(this.base64Image);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      this.fileName = null;
+    }
+  }
 }
--- src/app/modules/download-file/download-file.component.ts
+++ src/app/modules/download-file/download-file.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DownloadService } from '../../services/download.service';
+import { getErrorMessage, hideSpinner, showSpinner } from '../../services/functions.service';
+import { NotificationImplService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-download-file',
@@ -8,5 +11,11 @@ import { Component } from '@angular/core';
   styleUrl: './download-file.component.scss'
 })
 export default class DownloadFileComponent {
+  public download = inject(DownloadService);
+  public notificationService = inject(NotificationImplService);
 
+  async downloadFile() {
+    console.log(sessionStorage.getItem('dataUser'));
+    const result = await this.download.downloadFile();
+  }
 }
