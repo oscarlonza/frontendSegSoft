56bb2750005395c74fb2c3a74a6686c9b4ad9454
olondono <olonodono>
2024-09-20 15:17:15 -0500

    Se corrige apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://zoscarsecuresoftserver.azurewebsites.net',
+    apiUrl: 'https://zoscarsecuresoftserver.azurewebsites.net/api',
     accessToken: ''
 };
